fc27d5e26cee842f677044424d5a751fee4acf67
Melania Ion <melaniaion24@gmail.com>
2025-07-05 17:17:54 +0300

    add route locator

---
+++ gatewayservice/src/main/java/com/cinebloom/gatewayservice/GatewayRoutesConfig.java
@@ -0,0 +1,33 @@
+package com.cinebloom.gatewayservice;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class GatewayRoutesConfig {
+
+    @Bean
+    public RouteLocator customRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("mainservice_route", r -> r
+                        .path("/cinebloom/main/**")
+                        .filters(f -> f.rewritePath("/cinebloom/main/(?<segment>.*)", "/${segment}")
+                                .filter((exchange, chain) -> {
+                                    long start = System.currentTimeMillis();
+                                    return chain.filter(exchange).then(
+                                            Mono.fromRunnable(() -> {
+                                                long duration = System.currentTimeMillis() - start;
+                                                exchange.getResponse().getHeaders()
+                                                        .add("X-Response-Time", duration + "ms");
+                                            })
+                                    );
+                                })
+                        )
+                        .uri("lb://MAINSERVICE"))
+                .build();
+    }
+}
+
